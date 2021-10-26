c98fedd8fc875d60ee08f1b618f3761dfb8baaf2
yeabsera dessalegn <71721807+yeab-yeab@users.noreply.github.com>
2021-10-26 15:28:07 +0300

    Create starry night .py

---
+++ starry night .py
@@ -0,0 +1,28 @@
+import turtle as t
+from random import randint, random
+
+def draw_star(points, size, col, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    angle = 180 - (180 / points)
+    t.color(col)
+    t.begin_fill()
+    for i in range(points):
+        t.forward(size)
+        t.right(angle)
+    t.end_fill()
+
+# main code
+t.hideturtle()
+t.Screen().bgcolor('dark blue')
+t.speed(100)
+
+while True:
+    ranPts = randint(2, 5) * 2 + 1
+    ranSize = randint(10, 50)
+    ranCol = (random(), random(), random())
+    ranX = randint(-350, 300)
+    ranY = randint(-250, 250)
+ 
+    draw_star(ranPts, ranSize, ranCol, ranX, ranY)
